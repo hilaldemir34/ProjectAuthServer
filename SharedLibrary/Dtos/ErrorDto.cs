b645e1d7c95292504e035161fc2dedbf6358c787
Hilal Demir <126691643+hilaldemir34@users.noreply.github.com>
2023-05-20 23:27:44 +0300

    20.05.2023

--- SharedLibrary/Dtos/ErrorDto.cs
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -9,6 +9,22 @@ namespace SharedLibrary.Dtos
     public class ErrorDto
     {
         public List<string> Errors { get; private set; }
+        public bool IsShow { get; private set; }
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+        public ErrorDto(string error,bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = true;
+
+        }
+        public ErrorDto(List<string>errors,bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
 
     }
 }
